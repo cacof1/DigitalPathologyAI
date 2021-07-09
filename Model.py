03ba89345c35ee573ba13095464de2b005baf4f5
cacof1 <cacfek@gmail.com>
2021-07-09 10:09:41 +0100

    Updated the Model

--- Model.py
+++ Model.py
@@ -1,64 +1,90 @@
 import matplotlib.pyplot as plt
-from pytorch_lightning import LightningDataModule, LightningModule
+from pytorch_lightning import LightningDataModule, LightningModule, Trainer
+from torch.utils.data import DataLoader
+import segmentation_models_pytorch as smp
+from torch.utils.data import Dataset
 import numpy as np
 import torch
-import CLAM
-print(dir(CLAM))
+import openslide
+import h5py, sys
+from wsi_core.WholeSlideImage import WholeSlideImage
 
-"""
-### Data Loader
+### Dataset
+class DataGen(torch.utils.data.Dataset):
+   def __init__(self, file_path, mask_path, slide_path, transform=None):
+      super().__init__()
+      self.file_path  = file_path
+      self.mask_path  = mask_path
+      self.transform  = transform
+      self.coords     = h5py.File(file_path, "r")['coords']
+      self.wsi_object = WholeSlideImage(slide_path)
+   def __len__(self):
+      return self.coords.shape[0]
+
+   def __getitem__(self, idx):
+     patch = np.array(self.wsi_object.wsi.read_region(tuple(self.coords[idx]), 0, 256))
+     mask  = np.zeros(patch.shape)
+     ## Normalization
+     ## Transform
+
+     if self.transform:
+        patch = self.transform(patch)
+        mask  = self.transform(mask)
+     return image, mask
+
+### DataLoader
 class DataModule(LightningDataModule):
-  def __init__(self, image_array, mask_array, transform):
+  def __init__(self, file_path, mask_path, slide_path,transform = None,batch_size = 32):
     super().__init__()
-    self.image_array = image_array
-    self.mask_array = mask_array
-    self.transform = transform
-
-  def setup(self, stage: Optional[str] = None):
-
+    self.file_path  = file_path
+    self.mask_path  = mask_path
+    self.transform  = transform    
+    self.batch_size = batch_size
+    self.slide_path = slide_path
+  def setup(self, stage):
+    full_datasets = DataGen(self.file_path, self.mask_path, self.slide_path, self.transform)
+    datasplits    = np.round(np.array([0.7, 0.2, 0.1])*len(full_datasets)).astype(np.int32)
+    self.train_data, self.val_data,self.test_data =  torch.utils.data.random_split(full_datasets, datasplits)
   def train_dataloader(self):
-
+    return DataLoader(self.train_data, batch_size=self.batch_size)
   def val_dataloader(self):
-
-  def test_dataloader(self):    
+    return DataLoader(self.val_data, batch_size=self.batch_size)
+  def test_dataloader(self):
+    return DataLoader(self.test_data, batch_size=self.batch_size)
 
 ## Model
 class ModelRegression(LightningModule):
     def __init__(self) -> None:
       super().__init__()
-      from InnerEye.ML.models.architectures.classification.image_encoder_with_mlp import ImageEncoderWithMlp
-      self.model = ImageEncoderWithMlp()  
-      self.test_mse: List[torch.Tensor] = []
+      self.model = smp.FPN("resnet18", in_channels=4, classes=2, encoder_weights='imagenet')
+      self.loss_fcn = smp.losses.DiceLoss("multiclass", from_logits=True)
       
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # type: ignore
       return self.model(x)
     
-    def training_step(self, batch: Dict[str, torch.Tensor], *args: Any, **kwargs: Any) -> torch.Tensor:  # type: ignore
-      input = batch["x"]
-      target = batch["y"]
-      prediction = self.forward(input)
-      loss = 0#torch.nn.functional.mse_loss(prediction, target)
-      self.log("loss", loss, on_epoch=True, on_step=False)
+    def training_step(self, batch):
+      img, mask = batch
+      prediction = self.forward(img)
+      loss = self.loss_fcn(mask, prediction)
       return loss      
 
-    def validation_step(self, batch: Dict[str, torch.Tensor], *args: Any, **kwargs: Any) -> torch.Tensor:  # type: ignore
+    def validation_step(self, batch):
+      img, mask = batch
+      prediction = self.forward(img)
+      loss = self.loss_fcn(mask, prediction)
       return loss      
-    
 
-    def configure_optimizers(self) -> Tuple[List[Optimizer], List[_LRScheduler]]:
-      optimizer = Adam(self.parameters(), lr=1e-1)
-      scheduler = StepLR(optimizer, step_size=20, gamma=0.5)
+    def configure_optimizers(self):
+      optimizer = torch.optim.Adam(self.parameters(),lr=1e-1)
+      scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
       return [optimizer], [scheduler]
-
-    def on_test_epoch_start(self) -> None:
-      self.test_mse = []
-
-        
-    def on_test_epoch_end(self) -> None:
-        average_mse = torch.mean(torch.stack(self.test_mse))
-        Path("test_mse.txt").write_text(str(average_mse.item()))
 
 
 ## Main
-
-"""
+file_path  = sys.argv[1]
+mask_path  = sys.argv[2]
+slide_path = sys.argv[3]
+trainer = Trainer(gpus=1, max_epochs=5)
+model   = ModelRegression()
+data    = DataModule(file_path, mask_path,slide_path)
+trainer.fit(model, data)
---
+++ VisualiseContour.py
@@ -0,0 +1,37 @@
+import h5py
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+from wsi_core.WholeSlideImage import WholeSlideImage
+import geojson
+
+with open(sys.argv[1]) as f:
+    data = geojson.load(f)
+
+nContours = len(data)
+print(nContours)
+for n in range(nContours):
+    points = np.array(data[n]['geometry']['coordinates'])
+    wsi_object = WholeSlideImage(sys.argv[2])
+    print(dir(wsi_object))
+    vis_level = 0
+    dim = wsi_object.wsi.level_dimensions[vis_level]
+    points = np.int32(points/wsi_object.wsi.level_downsamples[vis_level])
+    img = np.array(wsi_object.wsi.read_region((0,0),vis_level,dim))
+    mask = np.zeros((dim[1],dim[0]))
+    cv2.fillConvexPoly(mask, np.int32(points), (255,255,255,255))
+    print(mask.shape)
+    
+    #plt.imshow(img)
+    #plt.imshow(mask,alpha=0.5)    
+    #plt.show()
+    #mask = mask.astype(bool)
+    print(np.max(mask))
+    #img[~mask] = (0,0,0,0)
+    #plt.imshow(img)
+    
+    #plt.show()
+
+
