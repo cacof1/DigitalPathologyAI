ca14ad800d3a86894cc31aae19516fc3916aacae
Mikaël Simard <mikael.simard5@gmail.com>
2021-11-08 08:58:21 +0000

    Adjusted paths for inference

--- TileDataset.sh
+++ TileDataset.sh
@@ -2,11 +2,11 @@
 rm -rf Preprocessing
 mkdir Preprocessing
 ## First remove contour
-python Application/Preprocess.py --source $1 --save_dir Preprocessing/ --patch_size 256 --step_size 256 --seg --patch --stitch 
+python Inference/Preprocess.py --source $1 --save_dir Preprocessing/ --patch_size 256 --step_size 256 --seg --patch --stitch 
 mkdir Preprocessing/wsi
 mv $1/*.svs Preprocessing/wsi
 
 ##Then tag those that are tumours
-python Application/Classify_tumor.py Preprocessing PretrainedModel/epoch\=02-val_acc\=1.00_tumour.ckpt
+python Inference/Classify_tumor.py Preprocessing PretrainedModel/epoch\=02-val_acc\=1.00_tumour.ckpt
 
 
