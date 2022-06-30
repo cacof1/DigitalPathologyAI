58f9b6fc7699f5fb9ba0bc7ed0dd827892c5d341
Mikaël Simard <mikael.simard5@gmail.com>
2022-06-30 15:26:41 +0100

    Multi-GPU support for training + inference.

---
+++ Dataloader.py
@@ -0,0 +1,194 @@
+from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+import openslide
+import torch
+from collections import Counter
+#import npyExportTools
+import itertools
+import Utils.sampling_schemes as sampling_schemes
+from Utils.OmeroTools import *
+from Utils import npyExportTools
+from pathlib import Path
+
+class DataGenerator(torch.utils.data.Dataset):
+
+    def __init__(self, tile_dataset, target="tumour_label", dim_list=[(256, 256)], vis_list=[0], inference=False, transform=None, target_transform=None):
+
+        super().__init__()
+        self.transform = transform
+        self.target_transform = target_transform
+        self.tile_dataset = tile_dataset
+        self.vis_list = vis_list
+        self.dim_list = dim_list
+        self.inference = inference
+        self.target = target
+
+    def __len__(self):
+        return int(self.tile_dataset.shape[0])
+
+    def __getitem__(self, id):
+        # load image
+        wsi_file = openslide.open_slide(self.tile_dataset["SVS_PATH"].iloc[id])
+
+        data_dict = {}
+        for dim in self.dim_list:
+            for vis_level in self.vis_list:
+                key = "_".join(map(str, dim)) + "_" + str(vis_level)
+                data_dict[key] = np.array(
+                    wsi_file.read_region([self.tile_dataset["coords_x"].iloc[id], self.tile_dataset["coords_y"].iloc[id]],
+                                         vis_level, dim).convert("RGB"))
+
+        ## Transform - Data Augmentation
+
+        if self.transform: data_dict = {key: self.transform(value) for (key, value) in data_dict.items()}
+        if (self.inference):
+            return data_dict
+
+        else:
+            label = self.coords[self.target].iloc[id]
+            if self.target_transform:
+                label = self.target_transform(label)
+            return data_dict, label
+
+
+class DataModule(LightningDataModule):
+
+    def __init__(self, tile_dataset, train_transform=None, val_transform=None, batch_size=8, n_per_sample=np.Inf,
+                 train_size=0.7, val_size=0.3, target=None, sampling_scheme='wsi', label_encoder=None, **kwargs):
+        super().__init__()
+
+        
+        self.batch_size = batch_size
+        tile_dataset[target] = label_encoder.transform(tile_dataset[target])
+
+        if sampling_scheme.lower() == 'wsi':
+            tile_dataset_sampled = sampling_schemes.sample_N_per_WSI(tile_dataset, n_per_sample=n_per_sample)
+            svi = np.unique(tile_dataset_sampled.SVS_PATH)
+            np.random.shuffle(svi)
+            train_idx, val_idx = train_test_split(svi, test_size=val_size, train_size=train_size)
+            tile_dataset_train = tile_dataset_sampled[tile_dataset_sampled.SVS_PATH.isin(train_idx)]
+            tile_dataset_valid = tile_dataset_sampled[tile_dataset_sampled.SVS_PATH.isin(val_idx)]
+
+        elif sampling_scheme.lower() == 'patch':
+            tile_dataset_sampled = sampling_schemes.sample_N_per_WSI(tile_dataset, n_per_sample=n_per_sample)
+            tile_dataset_train, tile_dataset_valid = train_test_split(tile_dataset_sampled,
+                                                                    test_size=val_size, train_size=train_size)
+
+        else:  # assume custom split
+            sampler = getattr(sampling_schemes, sampling_scheme)
+            tile_dataset_train, tile_dataset_valid = sampler(tile_dataset, target=target, n_per_sample=n_per_sample,
+                                                           train_size=train_size, test_size=val_size)
+
+        self.train_data = DataGenerator(tile_dataset_train, transform=train_transform, target=target, **kwargs)
+        self.val_data   = DataGenerator(tile_dataset_valid, transform=val_transform, target=target, **kwargs)
+        
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=self.batch_size, num_workers=10, pin_memory=True, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=self.batch_size, num_workers=10, pin_memory=True)
+
+def LoadFileParameter(config, dataset):
+
+    cur_basemodel_str = npyExportTools.basemodel_to_str(config)
+    tile_dataset = pd.DataFrame()
+    for npy_path in dataset.NPY_PATH:
+        existing_df = np.load(npy_path, allow_pickle=True).item()[cur_basemodel_str][1]
+        tile_dataset = pd.concat([tile_dataset, existing_df], ignore_index=True)
+
+    return tile_dataset
+
+def SaveFileParameter(config, df, SVS_ID):
+
+    cur_basemodel_str = npyExportTools.basemodel_to_str(config)
+    npy_path = os.path.join(config['DATA']['SVS_Folder'], 'patches', SVS_ID+".npy")
+    os.makedirs(os.path.split(npy_path)[0], exist_ok=True)  # in case folder is non-existent
+    npy_dict = np.load(npy_path, allow_pickle=True).item() if os.path.exists(npy_path) else {}
+    npy_dict[cur_basemodel_str] = [config, df]
+    np.save(npy_path, npy_dict)
+    return npy_path
+
+def QueryFromServer(config, **kwargs):
+    print("Querying from Server")
+    df   = pd.DataFrame()
+    conn = connect(config['OMERO']['Host'], config['OMERO']['User'], config['OMERO']['Pw'])  ## Group not implemented yet
+    conn.SERVICE_OPTS.setOmeroGroup('-1')
+    
+    keys = list(config['CRITERIA'].keys())
+    value_iter = itertools.product(*config['CRITERIA'].values())  ## Create a joint list with all elements
+    for value in value_iter:
+
+        query_base = """
+        select image.id, image.name, f2.size, a from 
+        ImageAnnotationLink ial
+        join ial.child a
+        join ial.parent image
+        left outer join image.pixels p
+        left outer join image.fileset as fs
+        left outer join fs.usedFiles as uf
+        left outer join uf.originalFile as f2     
+        """
+        query_end = ""
+        params = omero.sys.ParametersI()
+        for nb, temp in enumerate(value):
+            query_base += "join a.mapValue mv" + str(nb) + " \n        "
+            if nb == 0: query_end += "where (mv" + str(nb) + ".name = :key" + str(nb) + " and mv" + str(nb) + ".value = :value" + str(nb) + ")"
+            else:       query_end += " and (mv" + str(nb) + ".name = :key" + str(nb) + " and mv" + str(nb) + ".value = :value" + str(nb) + ")"
+            params.addString('key' + str(nb), keys[nb])
+            params.addString('value' + str(nb), temp)
+
+        query   = query_base + query_end
+        result  = conn.getQueryService().projection(query, params, {"omero.group": "-1"})
+
+        ## Version 1  -- populate only the criteria
+        """
+        df_criteria = pd.DataFrame([[row[0].val, Path(row[1].val).stem, row[2].val] for row in result], columns=["id_omero", "id_external", "Size"])                                  
+        for nb, temp in enumerate(value): df_criteria[keys[nb]]   = temp
+        """
+
+        ## Version 2 -- populate everything (fragile)
+        df_criteria = pd.DataFrame()            
+        for row in result: ## Transform the results into a panda dataframe for each found match
+            temp = pd.DataFrame([[row[0].val, Path(row[1].val).stem, row[2].val, *row[3].val.getMapValueAsMap().values()]],
+                                columns=["id_omero", "id_external", "Size", *row[3].val.getMapValueAsMap().keys()])
+            df_criteria = pd.concat([df_criteria, temp])                                    
+
+
+        df_criteria['SVS_PATH'] = [os.path.join(config['DATA']['SVS_Folder'], image_id+'.svs') for image_id in df_criteria['id_external']]
+        df_criteria['NPY_PATH'] = [os.path.join(config['DATA']['SVS_Folder'], 'patches', image_id+'.npy') for image_id in df_criteria['id_external']]
+        df = pd.concat([df, df_criteria])
+        
+    conn.close()
+    return df
+
+def SynchronizeSVS(config, df):
+    conn = connect(config['OMERO']['Host'], config['OMERO']['User'], config['OMERO']['Pw'])
+    conn.SERVICE_OPTS.setOmeroGroup('-1')
+
+    for index, image in df.iterrows():
+        filepath = image['SVS_PATH']
+        if os.path.exists(filepath):  # Exist
+            if not os.path.getsize(filepath) == image['Size']:  # Corrupted
+                print("SVS File size does not match - redownloading...")
+                os.remove(filepath)
+                download_image(image['id_omero'], config['DATA']['SVS_Folder'], config['OMERO']['User'], config['OMERO']['Host'], config['OMERO']['Pw'])
+                
+        else:  ## Doesn't exist
+            print("SVS File does not exist - downloading...")
+            download_image(image['id_omero'], config['DATA']['SVS_Folder'], config['OMERO']['User'], config['OMERO']['Host'], config['OMERO']['Pw'])
+            download_annotation(conn.getObject("Image", image['id_omero']), config['DATA']['SVS_Folder'])
+
+    conn.close()
+
+            
+def SynchronizeAnnotation(config, df):
+    conn = connect(config['OMERO']['Host'], config['OMERO']['User'], config['OMERO']['Pw'])
+    conn.SERVICE_OPTS.setOmeroGroup('-1')
+    
+    for index, image in df.iterrows(): 
+        if not os.path.exists(image['NPY_PATH']):  # Doesn't exist                        
+            download_annotation(conn.getObject("Image", image['id_omero']), config['DATA']['SVS_Folder'])
+            
+    conn.close()
---
+++ Preprocess.py
@@ -0,0 +1,127 @@
+from Dataloader.Dataloader import *
+from PreProcessing.PreProcessingTools import PreProcessor
+import toml
+from Utils import GetInfo
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+import pytorch_lightning as pl
+from torchvision import transforms
+import torch
+from QA.Normalization.Colour import ColourNorm
+from Model.ConvNet import ConvNet
+
+n_gpus = torch.cuda.device_count()  # could go into config file
+
+config = toml.load(sys.argv[1])
+########################################################################################################################
+# 1. Download all relevant files based on the configuration file
+
+SVS_dataset = QueryFromServer(config)
+SynchronizeSVS(config, SVS_dataset)
+print(SVS_dataset)
+########################################################################################################################
+# 2. Pre-processing: create npy files
+
+# option #1: preprocessor + save to npy
+preprocessor = PreProcessor(config)
+tile_dataset  = preprocessor.getTilesFromAnnotations(SVS_dataset)
+
+# option #2: load/save an existing preprocessing dataset
+# SaveFileParameter(config, tile_dataset)
+# tile_dataset = LoadFileParameter(config, SVS_dataset)
+config['DATA']['N_Classes'] = len(tile_dataset[config['DATA']['Label']].unique())
+########################################################################################################################
+# 3. Model 
+# Set up logging, model checkpoint
+name = GetInfo.format_model_name(config)
+if 'logger_folder' in config['CHECKPOINT']:
+    logger = TensorBoardLogger(os.path.join('lightning_logs', config['CHECKPOINT']['logger_folder']), name=name)
+else:
+    logger = TensorBoardLogger('lightning_logs', name=name)
+lr_monitor = LearningRateMonitor(logging_interval='step')
+checkpoint_callback = ModelCheckpoint(dirpath=config['CHECKPOINT']['Model_Save_Path'],
+                                      monitor=config['CHECKPOINT']['Monitor'],
+                                      filename=name + '-epoch{epoch:02d}-' + config['CHECKPOINT']['Monitor'] + '{' +
+                                      config['CHECKPOINT']['Monitor'] + ':.2f}',
+                                      save_top_k=1,
+                                      mode=config['CHECKPOINT']['Mode'])
+
+pl.seed_everything(config['ADVANCEDMODEL']['Random_Seed'])
+                   
+# transforms: augment data on training set
+if config['AUGMENTATION']['Rand_Operations'] > 0:
+    train_transform = transforms.Compose([
+        transforms.ToTensor(),
+        ColourNorm.Macenko(saved_fit_file=config['NORMALIZATION']['Colour_Norm_File']) if 'Colour_Norm_File' in config[
+            'NORMALIZATION'] else None,
+        transforms.ToPILImage(),
+        transforms.RandAugment(num_ops=config['AUGMENTATION']['Rand_Operations'],
+                               magnitude=config['AUGMENTATION']['Rand_Magnitude']),
+        # this only operates on 8-bit images (not normalised float32 tensors)
+        transforms.ToTensor(),  # this also normalizes to [0,1].,
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+else:
+    train_transform = transforms.Compose([
+        transforms.ToTensor(),  # this also normalizes to [0,1].,
+        ColourNorm.Macenko(saved_fit_file=config['NORMALIZATION']['Colour_Norm_File']) if 'Colour_Norm_File' in config[
+            'NORMALIZATION'] else None,
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+# transforms: colour norm only on validation set
+val_transform = transforms.Compose([
+    transforms.ToTensor(),  # this also normalizes to [0,1].
+    ColourNorm.Macenko(saved_fit_file=config['NORMALIZATION']['Colour_Norm_File']) if 'Colour_Norm_File' in config[
+        'NORMALIZATION'] else None,
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+trainer = pl.Trainer(gpus=n_gpus,
+                     benchmark=True,
+                     max_epochs=config['ADVANCEDMODEL']['Max_Epochs'],
+                     precision=config['BASEMODEL']['Precision'],
+                     callbacks=[checkpoint_callback, lr_monitor],
+                     logger=logger)
+
+le = preprocessing.LabelEncoder()
+le.fit(tile_dataset[config['DATA']['Label']])
+model = ConvNet(config, label_encoder=le)
+
+########################################################################################################################
+# 3. Dataloader
+tile_dataset['SVS_PATH'] = tile_dataset.apply(lambda row:SVS_dataset.loc[SVS_dataset['id_internal']==row['SVS_ID']]['SVS_PATH'],axis=1) #Stitch SVS Path local to tile_dataset
+data = DataModule(
+    tile_dataset,
+    batch_size=config['BASEMODEL']['Batch_Size'],
+    train_transform=train_transform,
+    val_transform=val_transform,
+    train_size=config['DATA']['Train_Size'],
+    val_size=config['DATA']['Val_Size'],
+    inference=False,
+    dim_list=config['BASEMODEL']['Patch_Size'],
+    vis_list=config['BASEMODEL']['Vis'],
+    n_per_sample=config['DATA']['N_Per_Sample'],
+    target=config['DATA']['Label'],
+    sampling_scheme=config['DATA']['Sampling_Scheme'],
+    label_encoder=le
+)
+
+# Give the user some insight on the data
+GetInfo.ShowTrainValTestInfo(data, config)
+"""
+config['DATA']['N_Training_Examples'] = data.train_data.__len__()
+config['DATA']['loss_weights'] = torch.ones(int(config['DATA']['N_Classes'])).float()
+
+The following will be used in an upcoming release to add weights to labels.
+N = sum(npatches_per_class)
+beta = (N-1)/N
+effective_samples = (1 - beta**npatches_per_class)/(1-beta)
+raw_scores = 1 / effective_samples
+w = config['DATA']['N_Classes'] * raw_scores / sum(raw_scores)
+config['DATA']['loss_weights'] = torch.tensor(w).float()
+print(config['DATA']['loss_weights'])
+* note: all the above could be moved directly into the ConvNet model or packaged in a function within Utils/
+* reference: Class-balanced Loss Based on Effective Number of Samples, Cui et al CVPR 2019.
+"""
+# Load model and train
+trainer.fit(model, data)
